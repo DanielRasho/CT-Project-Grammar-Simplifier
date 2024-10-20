b0ea7ca1204c056376d03de49d60e061924e8575
DanielRasho <ray22933@uvg.edu.gt>
2024-10-20 11:04:41 -0600

    feat: udpate production automate checker to accept new sintax with "{NON_TERMINAL}"

--- cmd/grammar/main.go
+++ cmd/grammar/main.go
@@ -1,81 +1,90 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	ast "github.com/DanielRasho/Computation-Theory/internal/abstract_syntax_tree"
+	nfaAutomata "github.com/DanielRasho/Computation-Theory/internal/nfa"
+	runner "github.com/DanielRasho/Computation-Theory/internal/runner_simulation"
+	shuttingyard "github.com/DanielRasho/Computation-Theory/internal/shuntingyard"
 )
 
 // REGEX DEFINITIONS
 const LETTERS string = "[abcdefghijklmnopqrstuvwxyz]"
 const CAPITAL_LETTERS string = "[ABCDEFGHIJKLMNOPQRSTUVWXYZ]"
 const DIGITS string = "[0123456789]"
+const NON_TERMINALS string = "\\{([ABCDEFGHIJKLMNOPQRSTUVWXYZ])+\\}"
 
 // PRODUCTIONS_REGEX for matching grammar productions
-var PRODUCTIONS_REGEX = fmt.Sprintf("%s -> ((%s|%s|%s|ε)+\\|)*(%s|%s|%s|ε)+",
+var PRODUCTIONS_REGEX = fmt.Sprintf("%s -> ((%s|%s|%s|%s|ε)+\\|)*(%s|%s|%s|%s|ε)+",
 	CAPITAL_LETTERS,
-	LETTERS, CAPITAL_LETTERS, DIGITS,
-	LETTERS, CAPITAL_LETTERS, DIGITS)
+	NON_TERMINALS, LETTERS, CAPITAL_LETTERS, DIGITS,
+	NON_TERMINALS, LETTERS, CAPITAL_LETTERS, DIGITS)
 
 func main() {
 
-	/*
-			filepath := "./input_data/grammars.txt"
-			file, err := os.Open(filepath)
-			if err != nil {
-				fmt.Printf("No se pudo abrir el archivo: %s.\n%v", filepath, err)
-				return
-			}
-			defer file.Close()
+	filepath := "./input_data/grammars.txt"
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Printf("No se pudo abrir el archivo: %s.\n%v", filepath, err)
+		return
+	}
+	defer file.Close()
 
-			scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-			// Creamos un NFA para validar las producciones
-			nfa := NFA_initializer()
+	// Creamos un NFA para validar las producciones
+	nfa := NFA_initializer()
 
-			var currentGrammar grammar.Grammar = make(grammar.Grammar)
-			grammarCounter := 1
+	// var currentGrammar grammar.Grammar = make(grammar.Grammar)
+	grammarCounter := 1
+	fmt.Println("\n=================================")
+	fmt.Printf("📝 Procesando gramática %d:\n", grammarCounter)
+	fmt.Println("=================================")
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+
+		// Detecta el delimitador que separa las gramáticas
+		if line == "---" {
+			grammarCounter++
 			fmt.Println("\n=================================")
 			fmt.Printf("📝 Procesando gramática %d:\n", grammarCounter)
 			fmt.Println("=================================")
-			for scanner.Scan() {
-				line := scanner.Text()
-				line = strings.TrimSpace(line)
+			continue
+			/*
+				grammar.SimplifyGrammar(&currentGrammar, true)
 
-				// Detecta el delimitador que separa las gramáticas
-				if line == "---" {
-					grammar.SimplifyGrammar(&currentGrammar, true)
-
-					// Preparar para la siguiente gramática
-					grammarCounter++
-					fmt.Println("\n=================================")
-					fmt.Printf("📝 Procesando gramática %d:\n", grammarCounter)
-					fmt.Println("=================================")
-					currentGrammar = make(grammar.Grammar)
-					continue
-				}
-
-				// Ignorar líneas vacías o comentarios
-				if line == "" || strings.HasPrefix(line, "#") {
-					continue
-				}
-
-				fmt.Printf("Checking %s :", line)
-				conclusion := runner.RunnerNFA(nfa, line)
-				if conclusion {
-					fmt.Printf(" is ✅\n")
-					currentGrammar.AddProduction(line)
-				} else {
-					fmt.Printf(" is ❌\n ERROR: incorrect grammar\n")
-					return
-				}
-			}
-			grammar.SimplifyGrammar(&currentGrammar, true)
+				// Preparar para la siguiente gramática
+				currentGrammar = make(grammar.Grammar)
+			*/
 		}
 
-		// Creates the NFA for checking if a production is valid
-		func NFA_initializer() *nfaAutomata.NFA {
-			_, postfix, _ := shuttingyard.RegexToPostfix(PRODUCTIONS_REGEX, false)
-			root := ast.BuildAST(postfix)
-			nfa := nfaAutomata.BuildNFA(root)
-			return nfa
-	*/
+		// Ignorar líneas vacías o comentarios
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		fmt.Printf("Checking %s :", line)
+		conclusion := runner.RunnerNFA(nfa, line)
+		if conclusion {
+			fmt.Printf(" is ✅\n")
+			// currentGrammar.AddProduction(line)
+		} else {
+			fmt.Printf(" is ❌\n ERROR: incorrect grammar\n")
+			return
+		}
+	}
+	// grammar.SimplifyGrammar(&currentGrammar, true)
+}
+
+// Creates the NFA for checking if a production is valid
+func NFA_initializer() *nfaAutomata.NFA {
+	_, postfix, _ := shuttingyard.RegexToPostfix(PRODUCTIONS_REGEX, false)
+	root := ast.BuildAST(postfix)
+	nfa := nfaAutomata.BuildNFA(root)
+	return nfa
 }
